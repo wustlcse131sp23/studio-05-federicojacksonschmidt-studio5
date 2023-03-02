1b360e24dc4f3c6c0fd4cc91d16cfd507a2354ce
fedbo <fedbo@10.232.184.231>
2023-03-02 12:52:40 -0600

    fff

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -110,8 +110,11 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
+		int[] values = new int [length]; // FIXME: Create an array of the appropriate size
 		// TODO: Finish this method
+		for (int i=0; i<length;i++) {
+			values= values[i]+ values [i];
+		}
 
 		
 
